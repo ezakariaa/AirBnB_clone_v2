e0e2dc10c1f3c7b6472ccf984eb5a8d493ac0300
ezakariaa <zakariaorche@gmail.com>
2024-07-11 17:34:24 -0700

    0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,57 +9,42 @@ import os.path
 env.hosts = ["100.27.4.173", "34.201.161.76"]
 env.user = 'ubuntu'
 
-def do_pack():
-    """
-    Empaqueter le contenu statique.
-    """
-    try:
-        directory_name = 'versions'
-        if not os.path.exists(directory_name):
-            local("mkdir -p {}".format(directory_name))
-        timestamp = time.strftime("%Y%m%d%H%M%S", time.gmtime())
-        file_name = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception as e:
-        print("Une erreur est survenue: {}".format(e))
-        return None
-
-
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    Déployer mon archive tgz sur mes serveurs.
-    """
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        base_name = file_name.split(".")[0]
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        remote_path = "/data/web_static/releases"
-        tmp_archive = "/tmp/{}".format(file_name)
-        target_path = "{}/{}".format(remote_path, base_name)
-
-        put(archive_path, tmp_archive)
-        run('mkdir -p {}/'.format(target_path))
-        run('tar -xzf {} -C {}/'.format(tmp_archive, target_path))
-        run('rm {}'.format(tmp_archive))
-        run('mv {0}/web_static/* {0}/'.format(target_path))
-        run('rm -rf {}/web_static'.format(target_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(target_path))
-        return True
-    except Exception as e:
-        print("Déploiement échoué: {}".format(e))
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-
-def deploy():
-    """
-    Empaqueter et déployer.
-    """
-    archive_path = do_pack()
-    if archive_path:
-        return do_deploy(archive_path)
-    else:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
