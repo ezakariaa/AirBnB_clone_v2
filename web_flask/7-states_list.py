bf7fa7802efb86ee3e2a4459f65f3b597f0fe038
ezakariaa <zakariaorche@gmail.com>
2024-07-29 00:39:31 +0200

    0x04. AirBnB clone - Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""Starts a Flask web application with listing of states."""
-from flask import Flask, render_template
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/states_list', methods=['GET'], strict_slashes=False)
+
+@app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
     states = storage.all("State")
-    sorted_states = sorted(all_states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Ferme la session de base de données."""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0")
